93b79732414e5bc3647477ddf1c45e2d05c88c31
arpit2707 <arpitgaurav.goldi723@gmail.com>
2019-09-15 22:37:54 +0530

    FirstError

--- JavaPrograms/Array1.java
+++ JavaPrograms/Array1.java
@@ -1,5 +1,4 @@
-
-
+package JavaPrograms;
 public class Array1{
     public static void main(String args[])
     {
--- JavaPrograms/JavaArray/ArraySum.java
+++ JavaPrograms/JavaArray/ArraySum.java
@@ -1,3 +1,4 @@
+package JavaPrograms.JavaArray;
 public class ArraySum{
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7,8};
